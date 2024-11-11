f19f6b77ed33e50125ad8fca65f3d017f7ecba3e
jokings2k <eromoselejonthan@gmail.com>
2024-11-11 09:17:15 +0100

    the new test

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
   } catch (error) {
     console.error("Error uploading to Cloudinary:", error);
     return NextResponse.json(
-      { error: error },
+      { error: error, apikey: process.env.CLOUDINARY_API_KEY },
       { status: 500 }
     );
   }
