3456d07599c1e015fa884fbe91431afb292c7ef9
jokings2k <eromoselejonthan@gmail.com>
2024-11-11 10:16:07 +0100

    env change

--- envs.ts
+++ envs.ts
@@ -1,2 +1,5 @@
 export const baseUrl = process.env.BASE_URL;
 export const jwtKey = process.env.JWT_KEY;export const cookieName="Toon_USER"
+export const cloundName = process.env.CLOUDINARY_CLOUD_NAME;
+export const apiKey = process.env.CLOUDINARY_API_KEY;
+export const apiSecret = process.env.CLOUDINARY_API_SECRET;
