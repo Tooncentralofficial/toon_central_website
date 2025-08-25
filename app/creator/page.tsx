9fbaf8290ca4ce44fe3ce73a1a82000cf6d1f5df
jokings2k <eromoselejonthan@gmail.com>
2025-08-25 12:28:57 +0100

    fixed the new

--- app/creator/page.tsx
+++ app/creator/page.tsx
@@ -1,11 +1,7 @@
 import { Metadata } from "next";
 import CreatorClient from "./creatorClient";
 
-export const metadata: Metadata = {
-  title: "Toon Central - Endless Comics, Endless stories.",
-  description:
-    " Read Anywhere. Create Anytime. Get started with us to unlock a world of comics and creativity",
-};
+
 
 const Page = () => {
   return <CreatorClient />;
