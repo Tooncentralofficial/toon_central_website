3e21f0c97293bc914231e21489ab3c11e9bd7428
jokings2k <eromoselejonthan@gmail.com>
2025-08-25 01:03:40 +0100

    added the new

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
         port: "",
       },
+      
     ],
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
